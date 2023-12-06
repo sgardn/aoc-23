300e779f7f800d360095ee2f933e3ddf4e58a983
Scott Gardner <sgardn04@gmail.com>
2023-12-06 11:14:20 -0500

    Day 4, part 2

--- lib/P04-scratchers/P04.ex
+++ lib/P04-scratchers/P04.ex
@@ -1,4 +1,6 @@
 defmodule Aoc23.P04 do
+  alias Aoc23.P04.TicketTally
+
   @input_file Path.join(["lib", "P04-scratchers", "input.txt"])
   @test_file Path.join(["lib", "P04-scratchers", "test.txt"])
   # @all_chars_regex ~r/\*|\&|\$|\-|\+|\%|\/|\#|\=|\@/
@@ -8,59 +10,89 @@ defmodule Aoc23.P04 do
   def input_file, do: @input_file
   def test_file, do: @test_file
 
-  # just
   defmodule Scratcher do
-    defstruct [:desired, :owned]
+    defstruct [:desired, :owned, :score]
+  end
+
+  defmodule TicketTally do
+    defstruct [:line_number, :line_score, :multiplier]
   end
 
 
   def run(path) do
     Aoc23.file_handle_to_lines(path)
-    |> Enum.map(fn l -> score_line(l) end)
+    |> Enum.map(fn l -> score_line(l, true) end)
     |> Enum.sum()
   end
 
-  def score_line(line) do
+  @spec score_line(binary(), any()) :: integer()
+  def score_line(line, exponential) do
     [_descriptor, number_str] = String.split(line, ": ")
+    s = parse_scratcher(number_str, exponential)
 
-    s = parse_scratcher(number_str)
-    IO.inspect(s)
-
-    score = score(s)
-    IO.puts("score: #{score} (line: #{line})")
-
-    score
+    s.score
   end
 
-  def parse_scratcher(number_str) do
+  @spec parse_scratcher(binary(), any()) :: %Aoc23.P04.Scratcher{
+          desired: list(),
+          owned: list(),
+          score: integer()
+        }
+  def parse_scratcher(number_str, exponential) do
     [desired, owned] = String.split(number_str, "|")
-    IO.puts("parse_scratcher(#{number_str})")
-    IO.puts("#{desired}, #{owned}")
-
-    %Scratcher{desired: parse_ticket_num_string(desired), owned: parse_ticket_num_string(owned)}
+    s = %Scratcher{desired: parse_ticket_num_string(desired), owned: parse_ticket_num_string(owned)}
+    %Scratcher{s | score: score(s, exponential)}
   end
 
+  @spec parse_ticket_num_string(binary()) :: list()
   def parse_ticket_num_string(str) do
-    IO.puts("parse_ticket_num_string(#{str})")
-    # IO.puts("#{desired}, #{owned}")
-    ns = String.split(str, " ")
+    String.split(str, " ")
     |> Enum.map(fn s -> String.trim(s) end)
     |> Enum.filter(fn s -> s != "" end)
-    IO.inspect(ns)
-
-    ns
     |> Enum.map(fn s -> to_integer!(s) end)
   end
 
-  def score(%Scratcher{desired: d, owned: o}) do
+  def score(%Scratcher{desired: d, owned: o}, exponential) do
     case num_overlapping(d, o) do
       0 -> 0
-      n -> 2 ** (n - 1)
+      n -> if exponential do 2 ** (n - 1) else n end
     end
   end
 
-  def run2(_path) do
-    0
+  def run2(path) do
+    tallies = Aoc23.file_handle_to_lines(path)
+    |> Enum.map(fn l -> score_line(l, false) end)
+    |> Enum.with_index(1)
+    |> Enum.map(fn {score, index} ->
+      %TicketTally{line_number: index, line_score: score, multiplier: 1}
+    end)
+
+    scored_tallies = (0..(length(tallies)-1))
+    |> Enum.to_list()
+    |> List.foldl(tallies, fn(index, tallies) ->
+      tally = Enum.at(tallies, index)
+      distribute_line_score(tally, tallies)
+    end)
+
+    IO.inspect(scored_tallies)
+
+    scored_tallies
+    |> Enum.reduce(0, fn el, acc ->
+      acc + el.multiplier
+    end)
+  end
+
+  def distribute_line_score(t, tallies) do
+    if t.line_score > 0 do
+      range = t.line_number..(t.line_number+t.line_score-1)
+
+      range
+      |> Enum.reduce(tallies, fn r, acc ->
+        List.update_at(acc, r, fn el -> %TicketTally{el | multiplier: el.multiplier + t.multiplier} end)
+      end)
+    else
+      tallies
+    end
   end
 
   @spec to_integer!(binary()) :: integer()
@@ -69,6 +101,7 @@ defmodule Aoc23.P04 do
     digits
   end
 
+  @spec num_overlapping(any(), any()) :: non_neg_integer()
   def num_overlapping(l1, l2) do
     MapSet.size(MapSet.intersection(MapSet.new(l1), MapSet.new(l2)))
   end
--- test/P04-scratchers/P04-scratchers_test.exs
+++ test/P04-scratchers/P04-scratchers_test.exs
@@ -1,46 +1,65 @@
 defmodule Aoc23Test.P04 do
   use ExUnit.Case
 
+  import Aoc23.P04
+  alias Aoc23.P04.TicketTally
+
   # @tag :focus
   # @tag :pending
-  describe "P04 part one (...)" do
+  describe "P04 part one (counting matches for tickets)" do
     test "test input" do
-      assert 13 == Aoc23.P04.run(Aoc23.P04.test_file())
+      assert 13 == run(test_file())
     end
 
     test "real input" do
-      assert 21558 == Aoc23.P04.run(Aoc23.P04.input_file())
+      assert 21558 == run(input_file())
     end
   end
 
-  describe "P0X part two (...)" do
+  describe "P04 part two (counting matches for tickets, and applying bonuses)" do
     test "test input" do
-      assert 30 == Aoc23.P04.run2(Aoc23.P04.test_file())
+      assert 30 == run2(test_file())
     end
 
     test "real input" do
-      refute 1 == 2
-      # assert 2101 == Aoc23.P02.run2(Aoc23.P02.input_file())
+      assert 10425665 == run2(input_file())
     end
   end
 
-  # describe "parsing structure" do
-  #   test "game ID can be pulled from a line" do
-  #     game = Aoc23.P02.parse_game(@test_line)
-  #     assert game.id == 1
-  #   end
 
-  #   test "the right number of rounds are parsed" do
-  #     game = Aoc23.P02.parse_game(@test_line)
-  #     assert length(game.rounds) == 3
-  #   end
+  @tt_one %TicketTally{line_number: 1, line_score: 1, multiplier: 1}
+  @tt_two %TicketTally{line_number: 2, line_score: 2, multiplier: 2}
+  @tt_three %TicketTally{line_number: 3, line_score: 0, multiplier: 1}
+  @tt_four %TicketTally{line_number: 4, line_score: 1, multiplier: 1}
 
-  #   test "games have the correct description of play" do
-  #     game = Aoc23.P02.parse_game(@test_line)
-  #     firstRound = Enum.at(game.rounds, 0)
-  #     assert length(firstRound.draws) == 2
-  #     assert Enum.member?(firstRound.draws, %Draw{color: "red", amount: 4})
-  #     assert Enum.member?(firstRound.draws, %Draw{color: "blue", amount: 3})
-  #   end
-  # end
+  @tallies [@tt_one, @tt_two, @tt_three, @tt_four]
+
+  describe "distribute_line_score" do
+    test "when a ticket doesn't score, shouldn't update followers" do
+      updated_tallies = distribute_line_score(@tt_three, @tallies)
+      assert updated_tallies == @tallies
+    end
+
+    test "when a ticket scores, should increment N following multipliers based on its multiplier" do
+      updated_tallies = distribute_line_score(@tt_one, @tallies)
+
+      assert updated_tallies == [
+        @tt_one,
+        %TicketTally{@tt_two | multiplier: 3},
+        @tt_three,
+        @tt_four,
+      ]
+    end
+
+    test "when a ticket scores AND has a multiplier, should add its multiplier to the following N multipliers" do
+      updated_tallies = distribute_line_score(@tt_two, @tallies)
+
+      assert updated_tallies == [
+        @tt_one,
+        @tt_two,
+        %TicketTally{@tt_three | multiplier: 3},
+        %TicketTally{@tt_four | multiplier: 3},
+      ]
+    end
+  end
 end
